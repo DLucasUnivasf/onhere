b72b20b188eff9bdd851acbf6e150ce73101d1c7
esron <esron.dtamar@gmail.com>
2017-03-02 21:39:54 -0300

    AlertDialog de esquecer a senha. Versão 0.1

--- app/src/main/java/galodamadrugada/onhere/LoginActivity.java
+++ app/src/main/java/galodamadrugada/onhere/LoginActivity.java
@@ -1,13 +1,58 @@
 package galodamadrugada.onhere;
 
+import android.content.DialogInterface;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    EditText  editTextPass, editTextEmail;
+    Button    buttonLogin, buttonSignInUp;
+    CheckBox  checkBoxRemember;
+    ImageView imageViewLogo;
+    TextView  textViewForgotPass;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        editTextEmail      = (EditText)  findViewById(R.id.editTextEmail);
+        editTextPass       = (EditText)  findViewById(R.id.editTextPass);
+        buttonLogin        = (Button)    findViewById(R.id.buttonLogin);
+        buttonSignInUp     = (Button)    findViewById(R.id.buttonSignInUp);
+        checkBoxRemember   = (CheckBox)  findViewById(R.id.checkBoxRemember);
+        imageViewLogo      = (ImageView) findViewById(R.id.imageViewLogo);
+        textViewForgotPass = (TextView)  findViewById(R.id.textViewForgotPass);
+
+        textViewForgotPass.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
+                LayoutInflater inflater = LoginActivity.this.getLayoutInflater();
+                builder.setView(inflater.inflate(R.layout.dialog_send_reset_password_email, null))
+                        .setTitle(R.string.send_reset_password_email_title)
+                        .setPositiveButton(R.string.send, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User clicked OK button
+                            }
+                        })
+                        .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog, int id) {
+                                // User cancelled the dialog
+                            }
+                        });
+
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
     }
 }
