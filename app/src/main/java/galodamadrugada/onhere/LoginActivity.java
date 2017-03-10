89f872939aa58a28928c28fb729349119b5f9156
esron <esron.dtamar@gmail.com>
2017-03-10 11:18:58 -0300

    Verificação de campos obrigatórios na tela de login

--- app/src/main/java/galodamadrugada/onhere/LoginActivity.java
+++ app/src/main/java/galodamadrugada/onhere/LoginActivity.java
@@ -1,5 +1,6 @@
 package galodamadrugada.onhere;
 
+import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
@@ -14,6 +15,8 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 public class LoginActivity extends AppCompatActivity {
+    final Context context = this;
+
     EditText  editTextPass, editTextEmail;
     Button    buttonLogin, buttonSignInUp;
     CheckBox  checkBoxRemember;
@@ -24,6 +27,7 @@ public class LoginActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
 
         editTextEmail      = (EditText)  findViewById(R.id.editTextEmail);
         editTextPass       = (EditText)  findViewById(R.id.editTextPass);
@@ -37,12 +41,16 @@ public class LoginActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 AlertDialog.Builder builder = new AlertDialog.Builder(LoginActivity.this);
-                LayoutInflater inflater = LoginActivity.this.getLayoutInflater();
-                builder.setView(inflater.inflate(R.layout.dialog_send_reset_password_email, null))
+                LayoutInflater inflater = LayoutInflater.from(context);
+                View view = inflater.inflate(R.layout.dialog_send_reset_password_email, null);
+
+                final EditText editTextResetEmail = (EditText)  view.findViewById(R.id.editTextResetEmail);
+
+                builder.setView(view)
                         .setTitle(R.string.send_reset_password_email_title)
                         .setPositiveButton(R.string.send, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int id) {
-                                // User clicked OK button
+                                // Não a nada aqui pois o método está sendo sobreescrito abaixo
                             }
                         })
                         .setNegativeButton(R.string.cancel, new DialogInterface.OnClickListener() {
@@ -51,8 +59,20 @@ public class LoginActivity extends AppCompatActivity {
                             }
                         });
 
-                AlertDialog dialog = builder.create();
+                final AlertDialog dialog = builder.create();
                 dialog.show();
+
+                dialog.getButton(AlertDialog.BUTTON_POSITIVE).setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        if (editTextResetEmail.getText().toString().equals("")) {
+                            editTextResetEmail.setError(getResources().getString(R.string.required_field));
+                        }
+                        else {
+                            dialog.dismiss();
+                        }
+                    }
+                });
             }
         });
 
