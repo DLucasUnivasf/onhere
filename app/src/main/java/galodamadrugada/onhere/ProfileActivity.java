01e83d0d24d84baf09d31fde57df693c1500a8d3
TallesRodrigues <tallesnr@gmail.com>
2017-03-14 09:46:30 -0300

    - tela de perfil concluída e  tela de evento iniciada

---
+++ app/src/main/java/galodamadrugada/onhere/ProfileActivity.java
@@ -0,0 +1,17 @@
+package galodamadrugada.onhere;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by UNIVASF on 14/03/2017.
+ */
+
+public class ProfileActivity  extends AppCompatActivity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_profile);
+    }
+}
